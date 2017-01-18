8396d53559a784c60055a22a650dfd8ef10ac6f1
The Creator <diegorazuri@gmail.com>
2017-01-18 17:57:51 -0500

    correccion en seccion userprofile, se dejo solo 1 ejemplo de viñetas de plan

--- src/client/components/itineraries.js
+++ src/client/components/itineraries.js
@@ -38,7 +38,7 @@ export default class Itineraries extends React.Component{
 									<span className="icon-credit-card icon-pay-fist"></span>
 									<p className="text-small-normal-grey-o">Cuota Personal</p>
 								</div>
-								<p className="text-small-normal-grey-o">S/. 283</p>
+								<p className="text-small-normal-grey-o">S/. 0</p>
 							</div>
 							<div className="detail-field">
 								<div>
@@ -46,7 +46,7 @@ export default class Itineraries extends React.Component{
 									<p className="text-small-normal-grey-o">Presupuesto total</p>
 								</div>
 								
-								<p className="text-small-normal-grey-o">S/. 1283</p>
+								<p className="text-small-normal-grey-o">S/. 0</p>
 							</div>
 							<div className="detail-field">
 								<div>
@@ -76,128 +76,7 @@ export default class Itineraries extends React.Component{
 							</div>
 						</div>
 					</div>
-					<div className="userprofile-wrapper-thumbnail">
-						<div className="userprofile-plan-thumbnail">
-							<figure>
-								<img src="style/img/Binndi_Workforus1.jpg"/>
-							</figure>
-							<div className="userprofile-plan-thumbnail-shadow"></div>
-							<div className="userprofile-plan-thumbnail-wrapper-button">
-								<span className="icon-cross"></span>
-								<h2 className="text-big-bold-white-m">nombre del plan</h2>
-								<a className="btn-edit-plan" href="">
-									<span className="icon-write text-small-normal-white"></span>
-									<p className="element-hidden text-small-normal-white">Ver</p>
-								</a>
-							</div>
-						</div>
-
-
-						<div className="userprofile-plan-thumbnail-detail">
-							<div className="detail-field">
-								<div>
-									<span className="icon-credit-card icon-pay-fist"></span>
-									<p className="text-small-normal-grey-o">Cuota Personal</p>
-								</div>
-								<p className="text-small-normal-grey-o">S/. 283</p>
-							</div>
-							<div className="detail-field">
-								<div>
-									<span className="icon-credit-card icon-pay-second"></span>
-									<p className="text-small-normal-grey-o">Presupuesto total</p>
-								</div>
-								
-								<p className="text-small-normal-grey-o">S/. 1283</p>
-							</div>
-							<div className="detail-field">
-								<div>
-									<span className="icon-calendar"></span>
-									<p className="text-small-normal-grey-o">Itinerario</p>
-								</div>
-								
-								<p className="text-small-normal-grey-o">8 actividades</p>
-							</div>
-							<div className="detail-field plan-friend-list">
-								<div>
-									<span className="icon-head"></span>
-									<p className="text-small-normal-grey-o">Amigos invitados</p>
-								</div>
-								
-								<ul>
-									<li>
-										
-									</li>
-									<li>
-										
-									</li>
-									<li>
-										
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div className="userprofile-wrapper-thumbnail">
-						<div className="userprofile-plan-thumbnail">
-							<figure>
-								<img src="style/img/Binndi_Workforus1.jpg"/>
-							</figure>
-							<div className="userprofile-plan-thumbnail-shadow"></div>
-							<div className="userprofile-plan-thumbnail-wrapper-button">
-								<span className="icon-cross"></span>
-								<h2 className="text-big-bold-white-m">nombre del plan</h2>
-								<a className="btn-edit-plan" href="">
-									<span className="icon-write text-small-normal-white"></span>
-									<p className="element-hidden text-small-normal-white">Ver</p>
-								</a>
-							</div>
-						</div>
-
-
-						<div className="userprofile-plan-thumbnail-detail">
-							<div className="detail-field">
-								<div>
-									<span className="icon-credit-card icon-pay-fist"></span>
-									<p className="text-small-normal-grey-o">Cuota Personal</p>
-								</div>
-								<p className="text-small-normal-grey-o">S/. 283</p>
-							</div>
-							<div className="detail-field">
-								<div>
-									<span className="icon-credit-card icon-pay-second"></span>
-									<p className="text-small-normal-grey-o">Presupuesto total</p>
-								</div>
-								
-								<p className="text-small-normal-grey-o">S/. 1283</p>
-							</div>
-							<div className="detail-field">
-								<div>
-									<span className="icon-calendar"></span>
-									<p className="text-small-normal-grey-o">Itinerario</p>
-								</div>
-								
-								<p className="text-small-normal-grey-o">8 actividades</p>
-							</div>
-							<div className="detail-field plan-friend-list">
-								<div>
-									<span className="icon-head"></span>
-									<p className="text-small-normal-grey-o">Amigos invitados</p>
-								</div>
-								
-								<ul>
-									<li>
-										
-									</li>
-									<li>
-										
-									</li>
-									<li>
-										
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
+					
 				</div>
 
 
